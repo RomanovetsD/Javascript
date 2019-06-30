f587576bfec57e8562126cfe23349994c4d965ea
RomanovetsD <hungry_hungry@ukr.net>
2019-06-30 07:19:08 +0300

    Added HW-4

--- Home_work/Module-3/1/js/app.js
+++ Home_work/Module-3/1/js/app.js
@@ -2,26 +2,19 @@ const logins = ['Mango', 'robotGoogles', 'Poly', 'Aj4x1sBozz', 'qwerty123'];
 /* Функция isLoginValid(login), проверка количества символов параметра login.
 Вернет true или false, попадает ли длина параметра в заданный диапазон
 от 4-х до 16-ти символов включительно. */
-const isLoginValid = function (login) {
-  const passed = login.length >= 4 && login.length <= 16;
-  if (passed) {
-    return true;
-  }
-  console.log('Ошибка! Логин должен быть от 4 до 16 символов');
 
-  return false;
+/* console.log('Ошибка! Логин должен быть от 4 до 16 символов'); */
+
+const isLoginValid = function (login) {
+  return login.length >= 4 && login.length <= 16;
 };
 
 /* Функция isLoginUnique(allLogins, login), которая принимает список всех логинов и добавляемый логин
  как параметры и проверяет наличие login в массиве allLogins,
  возвращая true если такого логина еще нет и false если логин уже используется. */
+
 const isLoginUnique = function (allLogins, login) {
-  const isInclude = allLogins.includes(login);
-  if (!isInclude) {
-    return true;
-  }
-  console.log('Такой логин уже используется!');
-  return false;
+  return allLogins.includes(login);
 };
 
 /* Функция addLogin добавляет или не добавляет логин в массив.
@@ -32,12 +25,8 @@ const addLogin = function (allLogins, login) {
   if (passed) {
     allLogins.push(login);
     console.log('Логин успешно добавлен!');
+    return true;
   }
 };
-
 // Вызовы функции для проверки
-addLogin(logins, 'Ajax'); // 'Логин успешно добавлен!'
-addLogin(logins, 'robotGoogles'); // 'Такой логин уже используется!'
-addLogin(logins, 'Zod'); // 'Ошибка! Логин должен быть от 4 до 16 символов'
-addLogin(logins, 'Zd'); // 'Ошибка! Логин должен быть от 4 до 16 символов'
-addLogin(logins, 'jqueryisextremelyfast'); // 'Ошибка! Логин должен быть от 4 до 16 символов'
+addLogin(logins, 'Ajakkkkk'); // 'Логин успешно добавлен!'
