f07ca2ac46032d06188da9b111cf5715e59420b3
RomanovetsD <hungry_hungry@ukr.net>
2019-09-13 19:54:28 +0300

    fix HW-12

--- Home_work/Module-12/src/js/utils/model.js
+++ Home_work/Module-12/src/js/utils/model.js
@@ -1,16 +1,16 @@
-import INITILAL_NOTES from "../../assets/notes.json";
-import Notepad from "../components/Notepad";
+import INITILAL_NOTES from '../../assets/notes.json';
+import Notepad from '../components/Notepad';
 import refs from './refs';
 import { renderNoteList } from './app';
 
 
 let notepad;
-if (localStorage.getItem("notes") !== null) {
-    let localNotes = JSON.parse(localStorage.getItem("notes"));
-    notepad = new Notepad(localNotes);
-  } else {
-    notepad = new Notepad(initialNotes);
-  }
+if (localStorage.getItem('notes') !== null) {
+  const localNotes = JSON.parse(localStorage.getItem('notes'));
+  notepad = new Notepad(localNotes);
+} else {
+  notepad = new Notepad(INITILAL_NOTES);
+}
 renderNoteList(refs.list, notepad.notes);
 
 export { notepad };
